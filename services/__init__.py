b241a309d3c2af25ccdbf9aa7ca166b8eb195a5d
joaoqvnnn <empresadefofocas@gmail.com>
2026-08-31 15:42:24 -0300

    Initialize services module with service imports

---
+++ services/__init__.py
@@ -0,0 +1,15 @@
+from services.balance import BalanceService
+from services.stock import StockService
+from services.purchase import PurchaseService
+from services.payment import PaymentService
+from services.affiliate import AffiliateService
+from services.giftcard import GiftCardService
+
+__all__ = [
+    "BalanceService",
+    "StockService",
+    "PurchaseService",
+    "PaymentService",
+    "AffiliateService",
+    "GiftCardService",
+]
