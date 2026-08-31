1f1160ef1b9eef338b4de742939b6182ca231918
joaoqvnnn <empresadefofocas@gmail.com>
2026-08-31 15:56:56 -0300

    Include admin routers in setup_routers
    Added admin routers to the setup_routers function.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -7,10 +7,13 @@ from handlers.client.wallet import router as wallet_router
 from handlers.client.profile import router as profile_router
 from handlers.client.affiliates import router as affiliates_router
 from handlers.client.extras import router as extras_router
+from handlers.admin import setup_admin_routers
 
 
 def setup_routers() -> Router:
     root = Router()
+
+    # Cliente
     root.include_router(start_router)
     root.include_router(catalog_router)
     root.include_router(purchase_router)
@@ -18,4 +21,8 @@ def setup_routers() -> Router:
     root.include_router(profile_router)
     root.include_router(affiliates_router)
     root.include_router(extras_router)
+
+    # Admin
+    root.include_router(setup_admin_routers())
+
     return root
