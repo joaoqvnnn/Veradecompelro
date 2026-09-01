8bcda6c4045cd3bae75f87ba518b0b87e2a066ca
joaoqvnnn <empresadefofocas@gmail.com>
2026-08-31 22:46:41 -0300

    Include logins router in admin routers
    Added logins router to the admin setup.

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,21 +1,3 @@
-from aiogram import Router
-
-from handlers.admin.panel import router as panel_router
-from handlers.admin.config import router as config_router
-from handlers.admin.products import router as products_router
-from handlers.admin.stock import router as stock_router
-from handlers.admin.users import router as users_router
-from handlers.admin.others import router as others_router
-from handlers.admin.messages import router as messages_router
-
-
-def setup_admin_routers() -> Router:
-    router = Router()
-    router.include_router(config_router)   # primeiro (dashboard + /admin)
-    router.include_router(panel_router)
-    router.include_router(products_router)
-    router.include_router(stock_router)
-    router.include_router(users_router)
-    router.include_router(others_router)
-    router.include_router(messages_router)
-    return router
+from handlers.admin.logins import router as logins_router
+# ...
+router.include_router(logins_router)
